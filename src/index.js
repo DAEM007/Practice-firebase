bf36664bc46abcac2e5f68bfbc0d8dc8a654e2df
EMMANUEL <dammylola299@gmail.com>
2022-07-11 01:38:51 +0100

    Second revision to database

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import {
     addDoc, deleteDoc, doc,
     query, where,
     orderBy, serverTimestamp,
-    getDoc
+    getDoc, updateDoc
 } from 'firebase/firestore';
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut, signInWithEmailAndPassword
+ } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDBGHOZbYEQz5ibnxTrzNpJFNCCYM5emDo",
@@ -20,7 +25,10 @@ const firebaseConfig = {
   initializeApp(firebaseConfig)
 
 // init services
-const db = getFirestore()
+const db = getFirestore() 
+
+// init authentication
+const auth = getAuth()
 
 // collection ref
 const colRef = collection(db, 'Books')
@@ -98,4 +106,68 @@ const docRef = doc(db, 'Books', '0lYuV3Em0UGuK6EBzixw')
 // real time listener for a single document
 onSnapshot(docRef, (doc) => {
   console.log(doc.data(), doc.id);
+})
+
+// updating a document
+const updateForm = document.querySelector('.update')
+updateForm.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'Books', updateForm.id.value)
+
+  updateDoc(docRef, {
+    author: "Andrew wommack"
+  })
+  .then(() => {
+    updateForm.reset()
+  })
+
+})
+
+// signing users up
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const email = signupForm.email.value
+  const password = signupForm.password.value
+ 
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log('User created:', cred.user);
+      signupForm.reset()
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+
+})
+
+// Logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('The user Logged out');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log('The user is Logged in', cred.user);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+
 })
